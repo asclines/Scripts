9180393656ab746b3d814ff24d73d152660cda54
Alexander Clines <injpk23@gmail.com>
2017-03-05 10:55:21 -0600

    Created script for setting up NVM on Ubuntu system

---
+++ scripts/setup-node-dev.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Sets up a Node.js development system on an Ubuntu system
+
+# Generic update
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+source ~/.bashrc
+
+# Setup NVM
+nvm install --lts
+nvm install node
+nvm use node
