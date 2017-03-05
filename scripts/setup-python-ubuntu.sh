4ca857a980cbdf00a40b22c273f086dd68bc7eb7
Alexander Clines <injpk23@gmail.com>
2017-03-05 11:08:58 -0600

    Updated python script to install pip for 2.7 and 3

--- scripts/setup-python-ubuntu.sh
+++ scripts/setup-python-ubuntu.sh
@@ -7,3 +7,10 @@ sudo apt-get upgrade
 
 # Install python 2 & 3
 sudo apt-get install python2.7 python3
+
+# Install pip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python2.7 get-pip.py
+sudo python3 get-pip.py
+
+rm get-pip.py
