c494feddff3b2c6f31728b638478d895fd7a3a14
Alexander Clines <injpk23@gmail.com>
2017-03-05 09:35:03 -0600

    Updated labels

--- scripts/setup-github-issue-labels.sh
+++ scripts/setup-github-issue-labels.sh
@@ -31,13 +31,16 @@ curl -H "Authorization: token $TOKEN" --include --request DELETE "https://api.gi
 curl -H "Authorization: token $TOKEN" --include --request DELETE "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels/wontfix"
 
 # Create labels
+curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Priority","color":"d93f0b"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
 curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Discussion","color":"CC317C"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
 curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Question","color":"CC317C"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
 curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Bug","color":"e74c3c"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
 curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Enhancement","color":"5EBEFF"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
 curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Feature Request","color":"91CA55"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
-curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Suport","color":"95a5a6"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
 curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Testing","color":"f1c40f"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
+curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Documentation","color":"FEF2C0"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
+curl -H "Authorization: token $TOKEN" --include --request POST --data '{"name":"Chore","color":"FEF2C0"}' "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
+
 
 
 # Language & Technology Specific, UNCOMMENT per project
