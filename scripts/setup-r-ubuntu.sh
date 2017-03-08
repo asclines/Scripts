44d99947bf8805ed656e683900d88b47cfad0f35
Alexander Clines <injpk23@gmail.com>
2017-03-08 13:39:01 -0600

    Fixed R setup script for multi ubuntu versions

--- scripts/setup-r-ubuntu.sh
+++ scripts/setup-r-ubuntu.sh
@@ -7,9 +7,17 @@ DIST_VERSION="$(lsb_release -a | grep Codename | cut -d$'\t' -f2)"
 # Add the cran source to sources
 
 # Authenticate
-sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu $DIST_VERSION/" >> /etc/apt/sources.list'
-gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
-gpg -a --export E084DAB9 | sudo apt-key add -
+if [ DIST_VERSION == "trusty" ]; then
+  sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list'
+  gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+  gpg -a --export E084DAB9 | sudo apt-key add -
+elif [ DIST_VERSION == "xenial" ]; then
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+  sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
+else
+  return 1;
+fi
+
 
 # Update and install R
 sudo apt-get update
