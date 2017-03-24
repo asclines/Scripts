06247ae9b255809ce3aa0a5109733f8ec0cfdc1d
Alexander Clines <injpk23@gmail.com>
2017-03-23 22:58:08 -0500

    Updated node set up script to us n as version manager instead of nvm

--- scripts/setup-node-ubuntu.sh
+++ scripts/setup-node-ubuntu.sh
@@ -6,13 +6,18 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Install NVM
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-source ~/.bashrc
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+#source ~/.bashrc
 
 # Setup NVM
-nvm install --lts
-nvm install node
-nvm use node
+#nvm install --lts
+#nvm install node
+#nvm use node
+
+# Setup N
+curl -L https://git.io/n-install | bash -s -- -y lts stable
+#curl -0 -L https://npmjs.com/install.sh | sudo sh # if npm isn't working
+source ~/.bashrc
 
 # Setup eslint
 npm install -g eslint eslint-plugin-chai-friendly
